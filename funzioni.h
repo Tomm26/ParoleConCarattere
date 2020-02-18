deaa41a2e0c3b4ad4721a987740431a8eff7cfc6
Tomm26 <52139893+Tomm26@users.noreply.github.com>
2020-02-18 18:16:17 +0100

    Create funzioni.h

---
+++ funzioni.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+using namespace std;
+struct lettere {
+	char lettera=' ';
+	int occ=1;
+};
+
+int ottieni_N(string nomefile);
+void caricaVettore(string nomefile, string v[]);
+void scambia(char &a, char &b);
+void ordinaParola(string &parola);
+
+void controllaOccorrenze(string parola);
